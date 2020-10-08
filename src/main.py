ebe5a3045f6d418dc0d855b691595f13c1a2b45b
Dylan Roy <dylan.roy@dowjones.com>
2020-10-09 00:10:09 +0200

    quick update to code

--- src/main.py
+++ src/main.py
@@ -10,5 +10,8 @@ def update_readme_medium_posts(medium_feed, readme_base, join_on):
     posts_joined = '\n'.join(posts)
     return readme_base[:readme_base.find(rss_title)] + f"{title}\n{posts_joined}"
 
-rss_title = "### Stories by Dylan Roy on Medium"
-updated_readme = update_readme_medium_posts("https://medium.com/feed/@dylanroy", readme_base, rss_title)
+rss_title = "### Stories by Dylan Roy on Medium" # Anchor for where to append posts
+readme = open('../README.md').read().close()
+updated_readme = update_readme_medium_posts("https://medium.com/feed/@dylanroy", readme, rss_title)
+with open('../README.md', "w+") as f:
+    f.write(updated_readme)
