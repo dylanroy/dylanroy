986d03e60b732b0a1c1057adbc33ac0903ce838e
Dylan Roy <dylan.k.roy@gmail.com>
2020-10-20 12:23:31 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,13 +11,12 @@ def update_footer():
 
 def update_readme_medium_posts(medium_feed, readme_base, join_on):
     d = feedparser.parse(medium_feed)
-    title = f"### {d['feed']['title']}"
     posts = []
     for item in d.entries:
         if item.get('tags'):
             posts.append(f" - [{item['title']}]({item['link']})")
     posts_joined = '\n'.join(posts)
-    return readme_base[:readme_base.find(rss_title)] + f"{title}\n{posts_joined}"
+    return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
 
 rss_title = "### Stories by Dylan Roy on Medium" # Anchor for where to append posts
 readme = Path('../README.md').read_text()
